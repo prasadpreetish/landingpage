e0451e56879ac3c20f49e4afeff004060d131a26
Preetish Rupa Prasad <www.preetishpd@gmail.com>
2022-06-23 13:03:30 +0530

    Added props to Blog.jsx

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -12,10 +12,10 @@ const Blog = () => {
             <hr />
         </div>
         <div className="blogpost--scroller">
-            <Postcard/>
-            <Postcard/>
-            <Postcard/>
-            <Postcard/>
+            <Postcard blogimgsrc="https://www.themezaa.com/html/pofo/images/latest-blog5.jpg" blogdate="25 April 2017  | " blogauth="JAY BENJAMIN" blogtitle="I like the body. I like to design everything to do with the body." link="https://www.themezaa.com/html/pofo/blog-post-layout-01.html"/>
+            <Postcard blogimgsrc="https://www.themezaa.com/html/pofo/images/latest-blog6.jpg" blogdate="20 April 2017  | " blogauth="HERMAN MILLER" blogtitle="Styles come and go. Design is a language, not a style." link="https://www.themezaa.com/html/pofo/blog-post-layout-02"/>
+            <Postcard blogimgsrc="https://www.themezaa.com/html/pofo/images/latest-blog7.jpg" blogdate="15 March 2017  | " blogauth="HUGH MACLEOD" blogtitle="Recognizing the need is the primary condition for design." link="https://www.themezaa.com/html/pofo/blog-post-layout-03"/>
+            <Postcard blogimgsrc="https://www.themezaa.com/html/pofo/images/latest-blog8.jpg" blogdate="10 March 2017  | " blogauth="JAY BENJAMIN" blogtitle="Get in over your head as often and as joyfully as possible." link="https://www.themezaa.com/html/pofo/blog-post-layout-04"/>
         </div>
     </div>
     </>
--- src/components/Postcard.jsx
+++ src/components/Postcard.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Postcard = () => {
+const Postcard = (props) => {
   return (
     <>
     <div className="post--card">
-        <img src="https://www.themezaa.com/html/pofo/images/latest-blog5.jpg" alt="Post 1" />
+        <img src={props.blogimgsrc} alt="Post 1" />
         <div className="post--date--author">
-            <div className="post--date faint--text">25 April 2017  |  </div>
-            <div className="post--author faint--text">JAY BENJAMIN</div>
+            <div className="post--date faint--text">{props.blogdate} </div>
+            <div className="post--author faint--text"> &nbsp;{props.blogauth}</div>
         </div>
-        <a href="https://www.themezaa.com/html/pofo/blog-post-layout-01.html" className="post--title b">I like the body. I like to design everything to do with the body.</a>
+        <a href={props.link} className="post--title b">{props.blogtitle}</a>
         <div className="post--cover--content faint--text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum text...</div>
     </div>
     </>
